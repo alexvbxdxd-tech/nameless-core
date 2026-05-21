8e45ca41c5fcc82c558ae05adddac3e0de19d5ed
alexvbxdxd-tech <alexvbxdxd@gmail.com>
2026-05-21 00:12:36 -0600

    Refactor token retrieval and message reply format

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import os
 import telebot
 
-TOKEN = "8817751804:AAEV74FF1o_MKMKs9a_EGlLSYDQdEk_BeiI"
+TOKEN = os.getenv("TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(m):
-    bot.reply_to(m, "⚡ NAMELESS CORE ONLINE")
+
+    bot.reply_to(
+        m.chat.id,
+        "⚡ NAMELESS CORE ONLINE"
+    )
 
 bot.infinity_polling()
